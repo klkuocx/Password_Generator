4d3d7594b8bd0c768ebcfc58592f2a763c9367e1
Kaleb Kuo <klkuo.cx@gmail.com>
2020-08-27 17:02:10 +0800

    Include Body-Parser to get form POST request

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Include packages and server related variables
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
@@ -8,8 +9,17 @@ const port = 3000
 app.set('view engine', 'handlebars')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 
+// Set body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // Set routes
 app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const options = req.body
+  console.log('options:', options)
   res.render('index')
 })
 
